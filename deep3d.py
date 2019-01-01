4f0849abd60169a39a6bf5c5414305edcc5f0627
ぺすちん <gp11a110@oecu.jp>
2019-01-01 09:42:15 +0900

    fix and clean

--- deep3d.py
+++ deep3d.py
@@ -2,39 +2,72 @@ import chainer
 import numpy as np
 import chainer.links as L
 import chainer.functions as F
-from chainer import cuda, optimizers, serializers, Variable
-import math
-import six
-from chainer.links.caffe.protobuf3 import caffe_pb2 as caffe_pb
-import cupy as cp
+import matplotlib.pyplot as plt
 
 
-def select(masks, left_image, left_shift=16):
-    '''
-    assumes inputs:
-        masks, shape N, H, W, S
-        left_image, shape N, C, H, W
-    returns
-        right_image, shape N, C, H, W
-    '''
+def select_layer(masks, left_image, left_shift=16):
+    """Use Selection Layer for applying mask to image.
 
+    Arguments:
+        masks {Variable} -- extracted feature by deep3d (shape N, H, W, S)
+        left_image {Variable} -- original left image (shape N, C, H, W)
+    returns:
+        predict right image {Variable} -- applyed mask to original left image (shape N, C, H, W)
+    """
     _, H, W, S = masks.shape
-    padded = F.pad(left_image, [[0,0],[0,0],[0,0],[left_shift, left_shift]], mode='constant')
-    b, ch, h, w = left_image.shape
-    pred = cp.zeros((b, ch, h, w), dtype=np.float32)
+    padded = F.pad(left_image,
+                   [[0, 0], [0, 0], [0, 0], [left_shift, left_shift]],
+                   mode='constant')
+
     for s in np.arange(S):
-        pad_slice = F.get_item(padded, (slice(None), slice(None), slice(0, H), slice(s, W+s)))
         mask_slice = masks[:, :, :, s]
         mask_slice = F.expand_dims(mask_slice, axis=1)
-        slided = F.broadcast_to(mask_slice, (b, ch, h, w)) * pad_slice
-        pred = slided + pred
-    return pred
+        pad_slice = F.get_item(padded, (slice(None), slice(None), slice(0, H), slice(s, W+s)))
+        if s == 0:
+            pred = F.expand_dims(pad_slice * mask_slice, axis=4)
+        else:
+            tmp = F.expand_dims(pad_slice * mask_slice, axis=4)
+            pred = F.concat([pred, tmp], axis=4)
+    return F.sum(pred, axis=4)
 
-# reference: 
-# https://github.com/piiswrong/deep3d/blob/e9433221662001717cfafe89c5f8a7e3b26fe1ee/sym.py
-# https://github.com/JustinTTL/Deep3D_TF/blob/master/Deep3D_Final.py
+
+def plot_argmax(masks, left, masked_left_images, right):
+    """Plot depth map, original left image, predict image, right image.
+
+    Arguments:
+        masks {Variable} -- extracted feature by deep3d
+        left {Variable} -- original left image
+        masked_left_images {Variable} -- applyed mask to original left image (predict right image)
+        right {Variable} -- right image
+    """
+    mask = chainer.cuda.to_cpu(masks.data[0, :, :, :])
+    fig = plt.figure(figsize=(18, 12))
+    fig.add_subplot(1, 4, 1)
+    plt.imshow(np.argmax(mask, axis=2), cmap='inferno')
+    fig.add_subplot(1, 4, 2)
+    plt.imshow(chainer.cuda.to_cpu(left[0, :, :, :]).transpose(1, 2, 0))
+    fig.add_subplot(1, 4, 3)
+    plt.imshow(chainer.cuda.to_cpu(masked_left_images.data[0, :, :, :]).transpose(1, 2, 0))
+    fig.add_subplot(1, 4, 4)
+    plt.imshow(chainer.cuda.to_cpu(right[0, :, :, :]).transpose(1, 2, 0))
+    plt.show()
+
+
 class Deep3D(chainer.Chain):
+    """Deep3D network architecture.
+
+    Reference:
+        https://github.com/piiswrong/deep3d/blob/e9433221662001717cfafe89c5f8a7e3b26fe1ee/sym.py
+        https://github.com/JustinTTL/Deep3D_TF/blob/master/Deep3D_Final.py
+    """
+
     def __init__(self, batchsize, size):
+        """Init Deep3D.
+
+        Arguments:
+            batchsize {integer} -- batchsize for learning
+            size {tuple} -- image size (width, height)
+        """
         super(Deep3D, self).__init__()
         self.batchsize = batchsize
         self.size = size
@@ -56,52 +89,108 @@ class Deep3D(chainer.Chain):
 
             scale = 1
             W, bias = self.get_initial_deconv_value(scale, 33, 33)
-            self.deconv1 = L.Deconvolution2D(33, 33, ksize=(1, 1), stride=(1, 1), pad=(0, 0), initialW=W, initial_bias=bias)
+            self.deconv1 = L.Deconvolution2D(33, 33, ksize=(1, 1),
+                                             stride=(1, 1),
+                                             pad=(0, 0),
+                                             initialW=W,
+                                             initial_bias=bias)
             scale *= 2
             W, bias = self.get_initial_deconv_value(2*scale, 33, 33)
-            self.deconv2 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale), stride=(scale, scale), pad=(scale//2, scale//2), initialW=W, initial_bias=bias)
+            self.deconv2 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale),
+                                             stride=(scale, scale), 
+                                             pad=(scale//2, scale//2),
+                                             initialW=W,
+                                             initial_bias=bias)
             scale *= 2
             W, bias = self.get_initial_deconv_value(2*scale, 33, 33)
-            self.deconv3 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale), stride=(scale, scale), pad=(scale//2, scale//2), initialW=W, initial_bias=bias)
+            self.deconv3 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale),
+                                             stride=(scale, scale),
+                                             pad=(scale//2, scale//2),
+                                             initialW=W,
+                                             initial_bias=bias)
             scale *= 2
             W, bias = self.get_initial_deconv_value(2*scale, 33, 33)
-            self.deconv4 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale), stride=(scale, scale), pad=(scale//2, scale//2), initialW=W, initial_bias=bias)
+            self.deconv4 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale),
+                                             stride=(scale, scale),
+                                             pad=(scale//2, scale//2),
+                                             initialW=W,
+                                             initial_bias=bias)
             scale *= 2
             W, bias = self.get_initial_deconv_value(2*scale, 33, 33)
-            self.deconv5 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale), stride=(scale, scale), pad=(scale//2, scale//2), initialW=W, initial_bias=bias)
+            self.deconv5 = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale),
+                                             stride=(scale, scale),
+                                             pad=(scale//2, scale//2),
+                                             initialW=W,
+                                             initial_bias=bias)
             scale = 2
             W, bias = self.get_initial_deconv_value(2*scale, 33, 33)
-            self.up_deconv_layer = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale), stride=(scale, scale), pad=(scale//2, scale//2), initialW=W, initial_bias=bias)
-            self.up_conv_layer = L.Convolution2D(33, 33, ksize=(3, 3), stride=1, pad=(1, 1))
-
+            self.up_deconv_layer = L.Deconvolution2D(33, 33, ksize=(2*scale, 2*scale),
+                                                     stride=(scale, scale),
+                                                     pad=(scale//2, scale//2),
+                                                     initialW=W,
+                                                     initial_bias=bias)
+            self.up_conv_layer = L.Convolution2D(33, 33, ksize=(3, 3),
+                                                 stride=1,
+                                                 pad=(1, 1))
 
     def __call__(self, left, right, original_left):
-        masked_left_images = self.predict(left, original_left)
+        """Calculate loss function by mean absolute error.
+
+        Arguments:
+            left {Variable} -- left image (BGR)
+            right {Variable} -- right image (RGB)
+            original_left {Variable} -- original left image (RGB)
+
+        Returns:
+            loss {Variable} -- loss value
+
+        """
+        masked_left_images, mask = self.predict(left, original_left)
+        if np.random.random() < 0.01:
+            plot_argmax(mask, original_left, masked_left_images, right)
         loss = F.mean_absolute_error(masked_left_images, right)
         chainer.report({'loss': loss}, self)
         return loss
 
     def predict(self, left, original_left):
+        """Predict right image.
+
+        Arguments:
+            left {Variable} -- left image (BGR)
+            original_left {Variable} -- original left image (RGB)
+
+        Returns:
+            masked_left_images {Variable} -- predicted right image
+            mask {Variable} -- mask
+
+        """
         mask = self.get_mask_value(left)
-        masked_left_images = select(mask, original_left)
-        return masked_left_images
+        masked_left_images = select_layer(mask, original_left)
+        return masked_left_images, mask
 
-    def get_mask_value(self, left):
-        bgr = left
-        #bgr = chainer.links.model.vision.vgg.prepare(left)
+    def get_mask_value(self, bgr):
+        """Get mask.
 
+        Arguments:
+            bgr {Variable} -- left image (BGR)
+
+        Returns:
+            mask {Variable} -- mask for obtaining prediction right image
+
+        """
         # VGG
-        vgg_result = self.vgg_layers(bgr, layers=['pool1', 'pool2', 'pool3', 'pool4', 'pool5'])
+        use_layers = ['pool1', 'pool2', 'pool3', 'pool4', 'pool5']
+        vgg_result = self.vgg_layers(bgr, layers=use_layers)
         vgg_fc6 = F.dropout(F.relu(self.fc6(vgg_result['pool5'])))
         vgg_fc7 = F.dropout(F.relu(self.fc7(vgg_fc6)))
         vgg_fc8 = F.dropout(F.relu(self.fc8(vgg_fc7)))
         
-        #-------branch 1-----
+        # -------branch 1-----
         bn_pool1 = self.batch_norm1(vgg_result['pool1'])
         branch1_1 = F.relu(self.branch_conv1(bn_pool1))
         branch1_2 = self.deconv1(branch1_1)
 
-        #-------branch 2-----
+        # -------branch 2-----
         bn_pool2 = self.batch_norm2(vgg_result['pool2'])
         branch2_1 = F.relu(self.branch_conv2(bn_pool2))
         branch2_2 = self.deconv2(branch2_1)
@@ -115,7 +204,7 @@ class Deep3D(chainer.Chain):
         bn_pool4 = self.batch_norm4(vgg_result['pool4'])
         branch4_1 = F.relu(self.branch_conv4(bn_pool4))
         branch4_2 = self.deconv4(branch4_1)
-        
+
         # -------branch 5-----
         # Upscaling last branch
         fc_RS = F.reshape(vgg_fc8, [self.batchsize, 33, int(self.size[1]/32), int(self.size[0]/32)])
@@ -126,10 +215,10 @@ class Deep3D(chainer.Chain):
         up_sum = branch1_2 + branch2_2 + branch3_2 + branch4_2 + branch5_2
         up = self.up_deconv_layer(up_sum)
         up = F.relu(up)
-        
+
         # Last Conv Layer
         up_conv = F.relu(self.up_conv_layer(up))
-        
+
         # Add + Mask + Selection
         mask = F.softmax(up_conv)
         mask = F.transpose(mask, (0, 2, 3, 1))
@@ -137,10 +226,25 @@ class Deep3D(chainer.Chain):
 
 
 # =========== Macro Layers =========== #
-    def get_initial_deconv_value(self, filter_size, in_channels, out_channels, 
+    def get_initial_deconv_value(self, filter_size, in_channels, out_channels,
                                  bias=True, initialization='bilinear'):
+        """Get initial weight by bilinear interpolation.
 
-        #Initializing to bilinear interpolation
+        Arguments:
+            filter_size {integer} -- filter size of CNN
+            in_channels {integer} -- input channels of CNN
+            out_channels {integer} -- output channels of CNN
+
+        Keyword Arguments:
+            bias {bool} -- whether use bias (default: {True})
+            initialization {str} -- initialization method (default: {'bilinear'})
+
+        Returns:
+            filters [numpy array] -- initial value of filter
+            biases [numpy array] -- initial value of biases
+
+        """
+        # Initializing to bilinear interpolation
         if initialization == 'bilinear':
             S = filter_size / 2
             C = (filter_size - 1 - (S % 2)) / (filter_size)
@@ -151,7 +255,7 @@ class Deep3D(chainer.Chain):
             filters = initial_value
         else:
             filters = None
-        
+
         biases = None
         if bias:
             biases = chainer.initializers.Normal(scale=0.01)
--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-# Dataset
 import os
 import numpy as np
 from PIL import Image
@@ -10,8 +9,6 @@ from chainer.dataset import dataset_mixin
 from chainer import training
 from chainer import iterators
 from chainer.training import extensions
-from chainer.backends import cuda
-
 from deep3d import Deep3D
 
 
@@ -25,7 +22,7 @@ def resize_image(img, size, vgg_prepare_flag=True):
     img = img.transpose(2, 0, 1)
     ch, h, w = img.shape
     offset_w = (w - size[0]) // 2
-    img = img[:,:,offset_w:offset_w+size[0]]
+    img = img[:, :, offset_w:offset_w+size[0]]
     return img
 
 
@@ -44,7 +41,7 @@ def _read_kitti_as_array(path_left, path_right, size=(768, 256)):
 
 def augument_random_crop(left, right, crop_size=(768, 256)):
     left = np.array(left, dtype=np.float32)
-    original_left = left.transpose(2, 0, 1)
+    original_left = np.copy(left.transpose(2, 0, 1))
     left = left[:, :, ::-1]
     left -= np.array([103.939, 116.779, 123.68], dtype=np.float32)
     left = left.transpose(2, 0, 1)
@@ -67,33 +64,10 @@ def augument_random_crop(left, right, crop_size=(768, 256)):
     return left, right, original_left
 
 
-def augment_image_pair(left_image, right_image):
-    # randomly shift gamma
-    random_gamma = np.random.uniform(0.8, 1.2)
-    left_image_aug  = left_image  ** random_gamma
-    right_image_aug = right_image ** random_gamma
-
-    # randomly shift brightness
-    random_brightness = np.random.uniform(0.5, 2.0)
-    left_image_aug  =  left_image_aug * random_brightness
-    right_image_aug = right_image_aug * random_brightness
-
-    # randomly shift color
-    random_colors = np.random.uniform(0.8, 1.2, 3)
-    left_image_aug  *= random_colors[:, None, None]
-    right_image_aug *= random_colors[:, None, None]
-
-    # saturate
-    left_image_aug  = np.clip(left_image_aug,  0., 1.)
-    right_image_aug = np.clip(right_image_aug, 0., 1.)
-
-    return left_image_aug, right_image_aug
-
-
 def prepare_dataset_path(folder_path):
     current_dir = Path.cwd()
-    left_datasets_dir = current_dir.parent / folder_path / 'left'
-    right_datasets_dir = current_dir.parent / folder_path / 'right'
+    left_datasets_dir = current_dir / folder_path / 'left'
+    right_datasets_dir = current_dir / folder_path / 'right'
     left_file_names = left_datasets_dir.glob('**/*.png')
     right_file_names = right_datasets_dir.glob('**/*.png')
 
@@ -104,15 +78,7 @@ def prepare_dataset_path(folder_path):
         path_right_imgs.append(str(r_img))
 
     paths = list(zip(path_left_imgs, path_right_imgs))
-    
     return paths
-
-
-def horizontal_flip(left, right, original_left):
-    left = left[:, :, ::-1]
-    right = right[:, :, ::-1]
-    original_left = original_left[:, :, ::-1]
-    return left, right, original_left
 
 
 class KittiDataset(dataset_mixin.DatasetMixin):
@@ -130,56 +96,51 @@ class KittiDataset(dataset_mixin.DatasetMixin):
 
     def get_example(self, i):
         path_left, path_right = self._paths[i]
-        path_left  = os.path.join(self._root, path_left).replace('.jpg', '.png')
+        path_left = os.path.join(self._root, path_left).replace('.jpg', '.png')
         path_right = os.path.join(self._root, path_right).replace('.jpg', '.png')
-        left, right, original_left = _read_kitti_as_array(path_left, path_right, size=(352, 256))
+        left, right, original_left = _read_kitti_as_array(path_left,
+                                                          path_right,
+                                                          size=(224, 224))
         if self.augmentation:
-            #if np.random.random() > 0.5:
-            #    left, right = augment_image_pair(left, right)
-            if np.random.random() > 0.5:
-                left, right, original_left = horizontal_flip(left, right, original_left)
+            pass
         left /= 255.
         right /= 255.
         original_left /= 255.
         return left, right, original_left
 
 
-def main():
+if __name__ == '__main__':
     iteration = 200000
     out_dir = './result/'
     batchsize = 16
     gpu = 0
-    size = (352, 256)
+    size = (224, 224)
 
     snapshot_interval = (2000, 'iteration')
-    report_interval = (10, 'iteration')
+    report_interval = (100, 'iteration')
     rate_change_trigger = (20000, 'iteration')
 
     model = Deep3D(batchsize, size)
     optimizer = chainer.optimizers.Adam(alpha=0.00002)
     optimizer.setup(model)
-    # optimizer.add_hook(chainer.optimizer_hooks.GradientNoise(0.01))
-    # optimizer.add_hook(chainer.optimizer.WeightDecay(1e-5))
 
-    train_paths = prepare_dataset_path('datasets')
+    train_paths = prepare_dataset_path('dataset')
     train_dataset = KittiDataset(train_paths, '')
-
-    train_iter = iterators.MultiprocessIterator(train_dataset, batchsize, repeat=True, shuffle=True, n_processes=4)
+    train_iter = iterators.MultiprocessIterator(train_dataset, batchsize,
+                                                repeat=True, shuffle=True,
+                                                n_processes=4)
 
     updater = training.StandardUpdater(train_iter, optimizer, device=gpu)
 
     trainer = training.Trainer(updater, (iteration, 'iteration'), out=out_dir)
-    trainer.extend(extensions.ExponentialShift('alpha', rate=0.1), trigger=rate_change_trigger)
+    trainer.extend(extensions.ExponentialShift('alpha', rate=0.1),
+                   trigger=rate_change_trigger)
     # trainer.extend(extensions.dump_graph('main/loss'))
     trainer.extend(extensions.snapshot(), trigger=snapshot_interval)
     trainer.extend(extensions.snapshot_object(
         model, 'model_iter_{.updater.iteration}'), trigger=snapshot_interval)
     trainer.extend(extensions.LogReport(trigger=report_interval))
-    trainer.extend(extensions.PrintReport(['iteration', 'main/loss', 'validation/main/loss']),
-                trigger=report_interval)
+    trainer.extend(extensions.PrintReport(['iteration', 'main/loss']),
+                   trigger=report_interval)
 
     trainer.run()
-
-
-if __name__ == '__main__':
-    main()
